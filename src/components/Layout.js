6554d5854cbbbbc265ec91983ff921d1c71a0fbf
Stackbit Code Editor <projects@stackbit.com>
2021-06-19 17:25:11 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -67,9 +67,9 @@ export default class Body extends React.Component {
                     <meta name="description" content={seoDescription} />
                     {!_.isEmpty(seoRobots) && <meta name="robots" content={seoRobots} />}
                     {seoExtra}
-                    <link rel="preconnect" href="https://fonts.gstatic.com"> 
-                    <link href="https://fonts.googleapis.com/css2?family=Viaoda+Libre&display=swap" rel="stylesheet">{favicon && <link rel="icon" href={withPrefix(favicon)} />}
-                    
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+                    {favicon && <link rel="icon" href={withPrefix(favicon)} />}
                     <body className={classNames(`palette-${palette}`)} />
                 </Helmet>
                 <div id="page" className="site">
